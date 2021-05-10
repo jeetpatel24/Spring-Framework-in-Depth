de6f070712794e5f0cd284bea57bbc12de9c9f91
jeetpatel24 <friend.jeet24@gmail.com>
2021-05-10 18:29:59 +0530

    Work with Environment/Env Variables

--- src/main/java/com/frankmoley/lil/fid/Application.java
+++ src/main/java/com/frankmoley/lil/fid/Application.java
@@ -9,12 +9,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Application {
 
+
     public static void main(String[] args) throws Exception {
         ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
         OutputService outputService = context.getBean(OutputService.class);
 
         for (int i=0;i<5;i++){
-            outputService.generateOutput("Frank");
+            outputService.generateOutput();
             Thread.sleep(5000);
         }
     }
