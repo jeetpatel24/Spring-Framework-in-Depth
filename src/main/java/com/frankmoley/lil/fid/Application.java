5e2c895f17172592f01a5c83e20da30a364e23d6
jeetpatel24 <friend.jeet24@gmail.com>
2021-05-15 11:27:44 +0530

    Implementing Component Scanning

--- src/main/java/com/frankmoley/lil/fid/Application.java
+++ src/main/java/com/frankmoley/lil/fid/Application.java
@@ -6,7 +6,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
-    
+
     public static void main(String[] args) throws Exception {
         ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
         OutputService outputService = context.getBean(OutputService.class);
